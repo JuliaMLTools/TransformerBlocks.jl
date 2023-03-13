36e5fac8b605849e1d96576a9f9f945e42306798
Julia MLTools <juliamltools@gmail.com>
2023-03-12 22:12:11 -0400

    debugging doc build process (3)

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter, TransformerBlocks
 makedocs(sitename="TransformerBlocks.jl")
 deploydocs(
+    dirname = "somedir",
     repo = "github.com/JuliaMLTools/TransformerBlocks.jl.git",
 )
